45e876de13114fea3f63aad4c6d37f83ea85b001
DavidRivasM <alonsorivasmora@gmail.com>
2021-02-08 23:19:22 +0000

    Versión 1

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,69 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import { func } from "prop-types";
 
 //create your first component
 export function Home() {
+	const [activo, setActivo] = useState("luzVerde");
+
+	const Seleccion = item => {
+		setActivo(item.target.className);
+	};
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="semaforo">
+				<div
+					className={
+						"luzRoja" + (activo === "luzRoja" ? "Activa" : "")
+					}
+					onClick={Seleccion}></div>
+
+				<div
+					className={
+						"luzNaranja" + (activo === "luzNaranja" ? "Activa" : "")
+					}
+					onClick={Seleccion}></div>
+
+				<div
+					className={
+						"luzVerde" + (activo === "luzVerde" ? "Activa" : "")
+					}
+					onClick={Seleccion}></div>
+			</div>
 		</div>
 	);
 }
+
+//Aca abajo esta la primera version del trabajo, sin el uso de hooks
+
+// export function Home() {
+// 	function Seleccion(evento) {
+// 		if (evento.target.className === "luzRoja") {
+//             evento.target.className = "luzRojaActiva";
+// 		} else if (evento.target.className === "luzRojaActiva") {
+// 			evento.target.className = "luzRoja";
+
+// 			//Luz Naranja
+// 		} else if (evento.target.className === "luzNaranja") {
+// 			evento.target.className = "luzNaranjaActiva";
+// 		} else if (evento.target.className === "luzNaranjaActiva") {
+// 			evento.target.className = "luzNaranja";
+
+// 			//Luz verde
+// 		} else if (evento.target.className === "luzVerde") {
+// 			evento.target.className = "luzVerdeActiva";
+// 		} else if (evento.target.className === "luzVerdeActiva") {
+// 			evento.target.className = "luzVerde";
+// 		}
+// 	}
+
+// 	return (
+// 		<div className="text-center mt-5">
+// 			<div className="semaforo">
+// 				<div className="luzRoja" onClick={Seleccion}></div>
+// 				<div className="luzNaranja" onClick={Seleccion}></div>
+// 				<div className="luzVerde" onClick={Seleccion}></div>
+// 			</div>
+// 		</div>
+// 	);
+// }
